a000eb8aab626f2c2736062c637cfba914ba97bc
nick <nickj8366@gmail.com>
2024-08-24 15:13:34 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="mt-11">
+    <h1 className="flex justify-center items-center text-xl">I am living on <span className="font-bold text-3xl ml-3">Github</span></h1>
+    </div>
+  );
+}
